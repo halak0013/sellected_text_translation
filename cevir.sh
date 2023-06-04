1c2947ecb7d6c2e26f67ad3c6d4d444e63d73436
halak0013 <zumruduanka0013@gmail.com>
2023-06-04 14:13:30 +0300

    location bug fixing

--- cevir.sh
+++ cevir.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 selected_text="$(xsel -o)"
-# fare konumunu al
+location=$(dirname "$0")
+# get mouse position
 xy=$(xdotool getmouselocation)
 
-# x ve y koordinatlarına ayır
+# split x and y coordinates
 x=$(echo $xy | awk '{print $1}' | awk -F: '{print $2}')
 y=$(echo $xy | awk '{print $2}' | awk -F: '{print $2}')
 
 echo $x
 
 if [ ! -z "$selected_text" ]; then
-    python3 cevir.py "$selected_text" $x $y
+    python3 $location/cevir.py "$selected_text" $x $y
 fi
