d4a2b17c40aa04068d89a8f2abac21078a63f25a
halak0013 <zumruduanka0013@gmail.com>
2023-06-06 09:20:03 +0300

    ui improvement

--- cevir.py
+++ cevir.py
@@ -3,6 +3,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk,Gdk
 import sys
 from googletrans import Translator
+import json
 
 class MainWindow(Gtk.Window):
     def __init__(self,x,y,selected_text):
@@ -10,10 +11,12 @@ class MainWindow(Gtk.Window):
 
         self.set_border_width(7)
         self.set_decorated(False)
+        self.set_keep_above(True)
         
         self.label = Gtk.Label()
         self.input_text=Gtk.Entry()
         self.cmb_lang=Gtk.ComboBoxText()
+        self.btn_copy=Gtk.Button(label="🗒")
         self.selected_text=selected_text
         self.connect("key-press-event", self.on_key_press_event)
         
@@ -38,7 +41,11 @@ class MainWindow(Gtk.Window):
             self.cmb_lang.append_text(l)
         self.cmb_lang.set_active(0)
         
+        self.btn_copy.connect("clicked", self.copy_text)
+        self.label.set_selectable(True)
+        
         input_box.add(self.input_text)
+        input_box.add(self.btn_copy)
         input_box.add(self.cmb_lang)
         
         main_box.add(input_box)
@@ -51,16 +58,42 @@ class MainWindow(Gtk.Window):
         self.label.set_text(text)
         
     def translate(self):
-        lang=self.cmb_lang.get_active_text().split()[0]
-        s_text=self.input_text.get_text()
-        translation = translator.translate(s_text, dest=lang).text
-        self.label.set_text(translation)
+        lang = self.cmb_lang.get_active_text().split()[0]
+        s_text = self.input_text.get_text()
+        if s_text is not None and s_text != "":
+            try:
+                translation = translator.translate(s_text, dest=lang).text
+                if translation is not None:
+                    self.label.set_text(self.text_configure(translation, 7))
+                else:
+                    self.label.set_text("Translation not available.")
+            except Exception as e:
+                print("Translation error:", e)
+                self.label.set_text("Translation error occurred.")
+        else:
+            self.label.set_text("No text to translate.")
+
+    def text_configure(self,txt,k):
+        txt=txt.split()
+        result=""
+        for i,t in enumerate(txt):
+            print(i%k)
+            if i%k==0:
+                result+="\n"
+            result+=t+" "
+        return result
+
+    def copy_text(self,widegt):
+        print("kopyla")
+        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        clipboard.set_text(self.label.get_text(), -1)
 
     def on_key_press_event(self, widget, event):
         if event.keyval == Gdk.KEY_Escape:
             Gtk.main_quit()
-        if event.keyval == Gdk.KEY_Return:
+        if event.keyval == Gdk.KEY_Return or event.keyval == Gdk.KEY_KP_Enter:
             self.translate()
+            print("çevir")
 
 if len(sys.argv) > 1:
     translator = Translator()
